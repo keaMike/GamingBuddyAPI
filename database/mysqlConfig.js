911fbe7933674c26d3bf3310d49f0c5f5aac76c6
L Ryholt <lucaryholt@gmail.com>
2021-10-28 17:58:58 +0200

    changes

--- database/mysqlConfig.js
+++ database/mysqlConfig.js
@@ -9,6 +9,7 @@ function initConnection() {
       user: process.env.MYSQL_DB_USERNAME,
       password: process.env.MYSQL_DB_PASSWORD,
       database: process.env.MYSQL_DB_NAME,
+      port: process.env.MYSQL_DB_PORT
     })
   }
 }

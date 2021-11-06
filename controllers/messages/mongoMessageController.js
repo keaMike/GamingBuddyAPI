655cde7873f10eee54128c6967bbdb0f1c894ae4
L Ryholt <lucaryholt@gmail.com>
2021-11-06 11:17:15 +0100

    messages

--- controllers/messages/mongoMessageController.js
+++ controllers/messages/mongoMessageController.js
@@ -5,15 +5,35 @@ const {
     verifyPassword,
     signToken,
 } = require('../../utils/encryption')
-const userCollection = 'users'
-const swipeCollection = 'swipes'
+const messageCollection = 'messages'
 
 exports.sendMessage = (req, res) => {
-    // TODO implement
-    return res.status(501).json({ data: 'Not yet implemented' })
+    const ownId = req.user.id
+    const receiverId = req.body.receiverId
+    const content = req.body.content
+
+    // FIXME check if it is correct
+
+    repo.insertOne(messageCollection, {
+      senderId: ownId,
+      receivingId: receiverId,
+      content,
+      insertedAt: 'now'
+    }).then(() => {
+      return res.status(201).json({ data: 'Message sent' })
+    }).catch(error => {
+      return res.status(500).json({ data: 'Could not sent message' })
+    })
 }
 
 exports.getAllMessages = (req, res) => {
-  // TODO implement
-  return res.status(501).json({ data: 'Not yet implemented' })
+  const { id } = req.user
+  
+  repo.find(messageCollection, {
+    $or: [ { senderId: id }, { receivingId: id } ]
+  }).then(results => {
+    return res.status(200).json({ data: results })
+  }).catch(error => {
+    return res.status(500).json({ data: `Something went wrong, please try again. ${error}` })
+  })
 }
--- controllers/messages/neo4jMessageController.js
+++ controllers/messages/neo4jMessageController.js
@@ -7,11 +7,62 @@ const {
 } = require('../../utils/encryption')
 
 exports.sendMessage = (req, res) => {
-    // TODO implement
-    return res.status(501).json({ data: 'Not yet implemented' })
+    const session = driver.session()
+    const ownId = req.user.id
+    const receiverId = req.body.receiverId
+    const content = req.body.content
+
+    session.run(
+      'MATCH ' +
+      '(sender:User), ' +
+      '(receiver:User) ' +
+      'WHERE ID(sender) = $senderIdParam ' +
+      'AND ID(receiver) = $receiverIdParam ' +
+      'CREATE (m:Message { content: $contentParam, sentAt: timestamp() }) ' +
+      'CREATE (sender)-[:SENT_MESSAGE]->(m) ' +
+      'CREATE (m)-[:SENT_TO]->(receiver)', 
+      {
+        senderIdParam: neo4j.int(ownId),
+        receiverIdParam: neo4j.int(receiverId),
+        contentParam: content
+      }
+    ).then(() => {
+      session.close()
+      return res.status(201).json({ data: 'Message sent' })
+    }).catch(error => {
+      session.close()
+      return res.status(500).json({ data: `Something went wrong, please try again. ${error}` })
+    })
 }
 
 exports.getAllMessages = (req, res) => {
-  // TODO implement
-  return res.status(501).json({ data: 'Not yet implemented' })
+  const session = driver.session()
+  const { id } = req.user
+
+  session.run(
+    'MATCH ' +
+    '(u:User), ' +
+    '(messages:Message) ' +
+    'WHERE ID(u) = $idParam ' + 
+    '((u)-[:SENT_MESSAGE]->(messages) OR ' +
+    '(messages)-[:SENT_TO]->(u)) ' +
+    'RETURN (messages)',
+    {
+      idParam: id
+    }
+    ).then(result => {
+      const resultData = []
+      result.records.forEach(record => {
+        const data = record._fields[0].properties
+        resultData.push({
+          content: data.content,
+          sentAt: data.sentAt
+        })
+      })
+      session.close()
+      return res.status(200).json({ data: resultData })
+    }).catch(error => {
+      session.close()
+      return res.status(500).json({ data: `Something went wrong, please try again. ${error}` })
+    })
 }

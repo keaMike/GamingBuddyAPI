f735e0f79b6d5c3125cd2cca5828f21b8be8686d
L Ryholt <lucaryholt@gmail.com>
2021-11-06 11:33:00 +0100

    mysql find matches

--- controllers/swipes/mysqlSwipeController.js
+++ controllers/swipes/mysqlSwipeController.js
@@ -6,8 +6,22 @@ const {
 } = require('../../utils/encryption')
 
 exports.findMatches = async (req, res) => {
-    // TODO implement
-    return res.status(501).json({ data: 'Not yet implemented' })
+  const { id } = req.user
+  const pool = await getPool()
+
+  try {
+    pool.query(
+      'SELECT * FROM users WHERE IsAMatch(?, users_id)',
+      [id],
+      (error, results) => {
+        if (error) throw error
+        return res.status(200).json({ data: results })
+      }
+    )
+  } catch (error) {
+    return res.status(500)
+    .json({ data: `Something went wrong, please try again` })
+  }
 }
 
 exports.swipeOnUser = async (req, res) => {

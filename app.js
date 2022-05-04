8a3e5b890026a8d92b4a9f3d343a805b7ff312b4
mikeeggertsen <m.eggertsen@hotmail.com>
2022-05-04 14:54:04 +0200

    Add cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 require('./database/mysqlConfig').initConnection()
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 app.use(express.static('public'))
+app.use(cors())
 
 const userRoutes = require('./routes/userRoutes')
 const swipeRoutes = require('./routes/swipeRoutes')

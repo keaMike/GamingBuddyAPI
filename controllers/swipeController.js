925b5144a68e406b6c196bbe3edf34a27075502d
mikeeggertsen <m.eggertsen@hotmail.com>
2022-05-16 07:18:35 +0200

    Refactor findMatches and prettier refac

--- controllers/swipeController.js
+++ controllers/swipeController.js
@@ -1,57 +1,74 @@
 const { driver } = require('../database/neo4jConfig')
-const { getUsersFromIds } = require('./userController')
+const neo4j = require('neo4j-driver')
+const userController = require('./userController')
 
 exports.findMatches = async (req, res) => {
-    const session = driver.session()
-    const { id } = req.user
+  const session = driver.session()
+  const skip = req.query.skip ? req.query.skip : 0
+  const limit = req.query.limit ? req.query.limit : 500
+  const { id } = req.user
 
-    session.run(
-        'MATCH ' +
-        '(u:SimpleUser), ' +
-        '(users:SimpleUser) ' +
-        'WHERE u.id = $idParam AND ' +
-        '(u)-[:HAS_SWIPED_ON { status: true }]->(users) AND ' +
-        '(users)-[:HAS_SWIPED_ON { status: true }]->(u) ' +
-        'RETURN (users)', 
-        {
-            idParam: id
-        }
-    ).then(result => {
-        const ids = []
-        result.records.forEach(record => {
-            const data = record._fields[0].properties
-            ids.push(data.id)
-        })
-        getUsersFromIds(ids, (results) => {
-            session.close()
-            if (results.length !== 0) {
-                return res.status(200).json({ data: results })
-            } else {
-                return res.status(404).json({ data: 'No matches' })
-            }
-        })
-    }).catch(error => {
+  session
+    .run(
+      `
+			MATCH  
+			(u:SimpleUser),  
+			(users:SimpleUser)  
+			WHERE u.id = $idParam AND  
+			(u)-[:HAS_SWIPED_ON { status: true }]->(users) AND  
+			(users)-[:HAS_SWIPED_ON { status: true }]->(u)  
+			RETURN (users)
+			SKIP $skip
+			LIMIT $limit
+    `,
+      {
+        idParam: id,
+        skip: neo4j.int(skip),
+        limit: neo4j.int(limit),
+      }
+    )
+    .then((result) => {
+      const ids = []
+      result.records.forEach((record) => {
+        const data = record._fields[0].properties
+        ids.push(data.id)
+      })
+      userController.getUsersFromIds(ids, (results) => {
         session.close()
-        return res.status(500).json({ data: `Something went wrong, please try again. ${error}` })
+        if (results.length !== 0) {
+          console.log(results)
+          return res.status(200).json({ data: results })
+        } else {
+          return res.status(200).json({ data: [] })
+        }
+      })
+    })
+    .catch((error) => {
+      session.close()
+      return res
+        .status(500)
+        .json({ data: `Something went wrong, please try again. ${error}` })
     })
 }
 
 exports.findUsersSwipedOn = async (id, callback) => {
-    const session = driver.session()
+  const session = driver.session()
 
-    await session.run(
-        'MATCH ' +
+  await session
+    .run(
+      'MATCH ' +
         '(u:SimpleUser), ' +
         '(users:SimpleUser) ' +
         'WHERE u.id = $idParam AND ' +
         '(u)-[:HAS_SWIPED_ON]->(users) ' +
-        'RETURN (users)', 
-        {
-            idParam: id
-        }
-    ).then(async (result) => {
+        'RETURN (users)',
+      {
+        idParam: id,
+      }
+    )
+    .then(async (result) => {
       const ids = []
-      result.records.forEach(record => {
+      result.records.forEach((record) => {
         const data = record._fields[0].properties
         ids.push(data.id)
       })
@@ -60,26 +77,30 @@ exports.findUsersSwipedOn = async (id, callback) => {
 }
 
 exports.swipeOnUser = async (req, res) => {
-    const session = driver.session()
-    const { id } = req.user
-    const otherUserId = req.body.otherUserId
-    const swipeStatus = req.body.status
+  const session = driver.session()
+  const { id } = req.user
+  const otherUserId = req.body.otherUserId
+  const swipeStatus = req.body.status
 
-    session.run(
-        'MERGE(u:SimpleUser { id: $idParam }) ' +
+  session
+    .run(
+      'MERGE(u:SimpleUser { id: $idParam }) ' +
         'MERGE(otherUser:SimpleUser { id: $otherUserIdParam }) ' +
-        'CREATE (u)-[:HAS_SWIPED_ON { timeStamp: timestamp(), status: $status }]->(otherUser)', 
-        {
-            idParam: id,
-            otherUserIdParam: otherUserId,
-            status: swipeStatus
-        }
-        ).then(() => {
-            session.close()
-            return res.status(200).json({ data: 'Swiped on user' })
-        }).catch(error => {
-            session.close()
-            return res.status(500).json({ data: `Something has gone wrong, please try again. ${error}` })
-        })
-    
+        'CREATE (u)-[:HAS_SWIPED_ON { timeStamp: timestamp(), status: $status }]->(otherUser)',
+      {
+        idParam: id,
+        otherUserIdParam: otherUserId,
+        status: swipeStatus,
+      }
+    )
+    .then(() => {
+      session.close()
+      return res.status(200).json({ data: 'Swiped on user' })
+    })
+    .catch((error) => {
+      session.close()
+      return res
+        .status(500)
+        .json({ data: `Something has gone wrong, please try again. ${error}` })
+    })
 }
